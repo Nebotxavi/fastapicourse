0be7c774eae640e0de8202dad9daa909cdf624bb
Nebotxavi <nebotxavi@gmail.com>
2022-08-26 06:59:22 +0200

    Deployment

--- app/routers/user.py
+++ app/routers/user.py
@@ -2,12 +2,13 @@ from fastapi import status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
 
 from .. import models, schemas, utils
-from ..database import  get_db 
+from ..database import get_db
 
 router = APIRouter(
     prefix='/users',
     tags=['Users']
 )
+
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserRes)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
@@ -22,12 +23,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
+
 @router.get("/{id}", response_model=schemas.UserRes)
-def get_user(id: int, db: Session= Depends(get_db)):
+def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"user with id: {id} was not found")
 
     return user
